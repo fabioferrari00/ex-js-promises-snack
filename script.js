421a276233c947f9ac344a98fd6f79b7b8141a3d
fabioferrari00 <fabioferarri.ff00@gmail.com>
2025-11-10 10:40:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Ottieni il titolo di un post con una Promise.
+//Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise 
+//che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+
+const getPostTitle = (id) => {
+    return new Promise((resolve, reject)=>{
+        fetch(`https://dummyjson.com/posts/${id}`)
+        .then(response => response.json())
+        .then(obj => resolve(obj.title))
+        .catch(reject)
+    })
+}
+
+getPostTitle(6)
+.then(obj => console.log(obj))
+.catch(error => console.error(error))
