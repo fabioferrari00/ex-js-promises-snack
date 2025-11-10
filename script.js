7fbc544d540379d0b24e466c280a4aac38ed8663
fabioferrari00 <fabioferarri.ff00@gmail.com>
2025-11-10 10:40:56 +0100

    snack 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//SNACK 1:
+
 //Ottieni il titolo di un post con una Promise.
 //Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise 
 //che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
